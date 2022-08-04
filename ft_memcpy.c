c7f5186c8233556515642bb1306f622b1a2f67c5
galdinomf <100026728+galdinomf@users.noreply.github.com>
2022-08-03 23:01:25 -0300

    Add files via upload

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgaldino <mgaldino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 15:57:53 by mgaldino          #+#    #+#             */
+/*   Updated: 2022/04/25 21:40:19 by mgaldino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int	i;
+
+	if ((dest == NULL) && (src == NULL))
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		*(char *)(dest + i) = *(char *)(src + i);
+		i++;
+	}
+	return (dest);
+}
